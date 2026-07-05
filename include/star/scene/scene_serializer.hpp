82870b415257ca35a9201559fcad9780038e5d47
Revolution <tavinho.daniel@gmail.com>
2026-07-05 19:30:05 -0300

    feat: update load_scene function signature to accept const Scene reference

--- include/star/scene/scene_serializer.hpp
+++ include/star/scene/scene_serializer.hpp
@@ -8,7 +8,7 @@ namespace star::scene {
     class Scene;
     
     [[nodiscard]] nlohmann::json serialize_scene(const Scene& scene);
-    void load_scene(Scene& scene, const nlohmann::json& document);
+    void load_scene(const Scene& scene, const nlohmann::json& document);
 
     [[nodiscard]] bool save_scene_to_file(Scene& scene, const std::filesystem::path& path);
     [[nodiscard]] bool load_scene_from_file(Scene& scene, const std::filesystem::path& path);
